815f4fb4fd2ee7bd59f61d371c916d0dac5e2297
Leandro Curioso <leandro.curioso@gmail.com>
2017-09-22 13:41:06 -0300

    New implementation binding loggers to req object.[Bug fixes]

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
-const LoggerDebug = require("./lib/logger-debug");
-const LoggerError = require("./lib/logger-error");
+const LoggerDebug   = require("./lib/logger-debug");
+const LoggerError   = require("./lib/logger-error");
 const LoggerRequest = require("./lib/logger-request");
-const LoggerInfo = require("./lib/logger-info");
-const LoggerWarn = require("./lib/logger-warn");
+const LoggerInfo    = require("./lib/logger-info");
+const LoggerWarn    = require("./lib/logger-warn");
 
 class Index {
 
-  static middleware(req, res, next){
-    req.loggers = (type = 'debug', options) => {
-      switch(type.toLowerCase()){
-        case 'debug':
-          logger = new LoggerDebug(req, res, options);
-        break;
-        case 'info':
-          logger = new LoggerInfo(req, res, options);
-        break;
-        case 'warn':
-          logger = new LoggerWan(req, res, options);
-        break;
-        case 'error':
-          logger = new LoggerError(req, res, options);
-        break;
+  /**
+   * Middleware for logger
+   * @param {Object} req Request object
+   * @param {Object} res Respons object
+   * @param {Function} next Next function
+   */
+  static middleware(req, res, next) {
+    req.logger = (type, log, options) => {
+      try {
+        let logger = null;
+        // Check if type is available
+        if (['debug', 'info', 'warn', 'error'].indexOf(type) === -1) {
+          throw new Error("Logger type not available, please use one of: ['debug', 'info', 'warn', 'error']");
+        }
+        switch (type.toLowerCase()) {
+          default:
+          case 'debug':
+            logger = new LoggerDebug(req, res, options).execute(log);
+            break;
+          case 'info':
+            logger = new LoggerInfo(req, res, options).execute(log);
+            break;
+          case 'warn':
+            logger = new LoggerWarn(req, res, options).execute(log);
+            break;
+          case 'error':
+            logger = new LoggerError(req, res, options).execute(log);
+            break;
+        }
+        return logger;
+      } catch (err) {
+        throw err;
       }
-      next();
-    }
+    };
+    next();
   }
 
+  /**
+   * Middleware for request logging
+   * @param {Object} options Options object
+   * @return {Function} Execute function
+   */
   static requestMiddleware(options) {
-      return new LoggerRequest(options).execute();
+    return new LoggerRequest(options).execute;
   }
 
 }
