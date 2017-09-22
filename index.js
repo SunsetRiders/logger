5b7e838ba539143e65cdef368059ad9f5bc22b47
Leandro Curioso <leandro.curioso@gmail.com>
2017-09-22 13:39:31 -0300

    New implementation binding loggers to req object.

--- index.js
+++ index.js
@@ -6,16 +6,30 @@ const LoggerWarn = require("./lib/logger-warn");
 
 class Index {
 
-  constructor() {
-    return {
-        LoggerDebug,
-        LoggerError,
-        LoggerRequest,
-        LoggerInfo,
-        LoggerWarn
+  static middleware(req, res, next){
+    req.loggers = (type = 'debug', options) => {
+      switch(type.toLowerCase()){
+        case 'debug':
+          logger = new LoggerDebug(req, res, options);
+        break;
+        case 'info':
+          logger = new LoggerInfo(req, res, options);
+        break;
+        case 'warn':
+          logger = new LoggerWan(req, res, options);
+        break;
+        case 'error':
+          logger = new LoggerError(req, res, options);
+        break;
+      }
+      next();
     }
+  }
+
+  static requestMiddleware(options) {
+      return new LoggerRequest(options).execute();
   }
 
 }
 
-module.exports = new Index();
+module.exports = Index;
